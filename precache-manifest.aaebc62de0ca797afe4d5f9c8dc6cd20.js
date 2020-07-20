72ab21c9de0a482385b071cf6af8643b867eec84
Alexei Moisseev <alexmois@ya.ru>
2020-07-20 08:19:55 +0300

    Live demo

---
+++ precache-manifest.aaebc62de0ca797afe4d5f9c8dc6cd20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9568d5b67f45a4058514c354ade26e7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "158438e80b7ecc2ca29f",
+    "url": "/static/css/main.d78d4d91.chunk.css"
+  },
+  {
+    "revision": "867a1a3aee93c6b98d5e",
+    "url": "/static/js/2.27fac97c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.27fac97c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "158438e80b7ecc2ca29f",
+    "url": "/static/js/main.5ccd69f4.chunk.js"
+  },
+  {
+    "revision": "0efb93943a4e43b31669",
+    "url": "/static/js/runtime-main.e86f8d92.js"
+  }
+]);
