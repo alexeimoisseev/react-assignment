19a0fd7f6ffa7f95568bb0eff6a2f840c4919eda
Alexei Moisseev <alexmois@ya.ru>
2020-07-20 08:37:36 +0300

    Updates

---
+++ precache-manifest.0e42b16c57b2dd270db56ea8b5204020.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88d3d18df2323697c3339c062aed8b9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7992e7250b46b5d08847",
+    "url": "/static/css/main.d78d4d91.chunk.css"
+  },
+  {
+    "revision": "c21af28f878301dd5fae",
+    "url": "/static/js/2.26ede571.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.26ede571.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7992e7250b46b5d08847",
+    "url": "/static/js/main.c6835491.chunk.js"
+  },
+  {
+    "revision": "a88d15e1174fe8b2ea32",
+    "url": "/static/js/runtime-main.318bc420.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://alexeimoisseev.github.io/react-assignment/precache-manifest.1c8cc66506d5841e383cabd246f97b3f.js"
+  "/precache-manifest.0e42b16c57b2dd270db56ea8b5204020.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://alexeimoisseev.github.io/react-assignment/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
